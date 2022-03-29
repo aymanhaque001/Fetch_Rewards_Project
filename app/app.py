eebbb984e7229d645cf21e10ae9ac6246e7ecba7
Ayman Haque <aymanhaque001@gmail.com>
2022-03-29 03:18:59 -0500

    Change harcoded validation rules using regex to pydantic schema

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple, Dict
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import uvicorn
 from main import *
@@ -9,31 +9,42 @@ from main import ImageGeneration
 
 app = FastAPI()
 
-# I was forced to use the inputs as strings because of already setting up a strict validation
-# method using regex. Alternatively, I could have used List[tuples] in the Schema
-# for Corner_Points. Apologies for this inconvenience.
+
+def validate_schema(dimensions, corner_points):
+    if dimensions[0] < 2 or dimensions[1] < 2:
+        raise HTTPException(status_code=422, detail="Format error")
+        return -1
+
+    if len(corner_points) != 4:
+        raise HTTPException(status_code=422, detail="Format error")
+        return -1
+
+    for i in corner_points:
+        if len(i) != 2 and (i[0].isnumeric() == False or i[1].isnumeric() == False):
+            raise HTTPException(status_code=422, detail="Format error")
+            return -1
+    return True
 
 
 class schema(BaseModel):
-    Dimensions: str
-    Corner_Points: str
+    Dimensions: tuple[int, int]
+    Points: list[tuple[float, float]]
 
 
 @app.get("/")
 async def root():
-    return {"message": "This is the Fetch_rewards project."}
+    return {"message": "This is a project."}
 
 
 @app.post("/getvals")
 def tup(ValDict: schema):
-    Dimensions = InputDimensions(ValDict.Dimensions)
-    Corner_Points = InputCoordinates(ValDict.Corner_Points)
-    Image_obj = image(Dimensions, Corner_Points)
-    Image = ImageGeneration(Image_obj)
-    Solution_mtrx = Image.matrix_generation()
-    print(Solution_mtrx)
-    return{"Solution Matrix": Solution_mtrx,
-           "Dimensions": ValDict.Dimensions}
+    if validate_schema(ValDict.Dimensions, ValDict.Points):
+        Image_obj = image(ValDict.Dimensions, ValDict.Points)
+        Image = ImageGeneration(Image_obj)
+        Solution_mtrx = Image.matrix_generation()
+        print(Solution_mtrx)
+        return{"Solution Matrix": Solution_mtrx,
+               "Dimensions": ValDict.Dimensions}
 
 
 if __name__ == '__main__':
--- app/main.py
+++ app/main.py
@@ -1,49 +1,3 @@
-import re
-
-
-# Input functions for dimensions and Corner_Points
-def InputDimensions(user_input):
-    # => Function for taking the input value for dimension. Returns a tuple in format (X,Y). Confirms if format is correct
-    # => before returning.
-    # user_input = input("Enter Dimensions in format -> (X,Y): ")
-    pattern = "(\(\d+\,\d+\))"
-    if re.match("(\(\d+\,\d+\))", user_input):
-        print("Accepted")
-        dimensions = []
-        for i in user_input:
-            if i not in [")", "(", ","]:
-                dimensions.append(int(i))
-        return dimensions
-
-    else:
-        raise Exception("Error : Incorrect Format. Try again.")
-
-
-def InputCoordinates(s):
-    # Function for taking the coordinates for the corner points . Returns a tuple in format [(X,Y),(I,J),(M,N),(P,Q)]
-    # All numbers must be in floats. Confirms if format is correct before returning.
-    # s = input(
-    #     "Enter Coordinates in the following format e.g.[(1.5, 1.5),(4.0, 1.5),(1.5, 8.0),(4.0, 8.0)] ,: ")
-    Corner_Points = []
-    pattern = '\[\(\d+.\d+\, \d+.\d+\)\,\(\d+.\d+\, \d+.\d+\)\,\(\d+.\d+\, \d+.\d+\)\,\(\d+.\d+\, \d+.\d+\)\]'
-    if re.match(pattern, s):
-        print('True')
-        string = " "
-        for i in s:
-            if i not in ["[", "]", ")", "(", ]:
-                string = string+i
-
-        string = string.split(",")
-        k = []
-        for i in range(len(string)):
-            k.append(float(string[i]))
-        for i in range(1, len(k), 2):
-            j = (k[i-1], k[i])
-            Corner_Points.append(j)
-        return Corner_Points
-    else:
-        raise Exception("Error: Incorrect Format")
-
 
 # A seperate class for image. Stores the corner points and dimensions.
 
@@ -96,13 +50,17 @@ class ImageGeneration:
                 upperblock.append(i)
             else:
                 lowerblock.append(i)
+
         return upperblock, lowerblock
 
     def matrix_generation(self):
+        print(self.row)
         if self.validate_corner_points() == True:
             up, low = self.settingbounds()
-            x_inc = (abs(up[0][0] - up[1][0]) / (self.column-1.0))
-            y_inc = (abs(up[0][1] - low[0][1]) / (self.row-1.0))
+            x_inc = (abs(up[0][0] - up[1][0]) / (self.column-1))
+            y_inc = (abs(up[0][1] - low[0][1]) / (self.row-1))
+            x_inc = round(x_inc, 2)
+            y_inc = round(y_inc, 2)
 
             solution = []
 
